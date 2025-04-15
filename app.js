11d5b739456792b89b3183a7d092e2e9e662f0cb
Aria <ARYAir.121212@gmail.com>
2025-04-15 21:17:38 +0330

    first comit

---
+++ app.js
@@ -0,0 +1,26 @@
+let input1 = document.getElementById("input1")
+let input2 = document.getElementById("input2")
+let massege = document.getElementById("Alert")
+let container = document.querySelector(".container")
+let button = document.getElementById("button")
+
+
+
+
+let buttonFunk = function () {
+    if (input1.value.length < 12 || input1.value.length < 8) {
+        massege.style.display = "block"
+        container.style.boxShadow = "0px 0px 50px red";
+        button.style.backgroundColor = "red"
+    } else {
+        massege.style.display = "none"
+        container.style.boxShadow = "0px 0px 0px red";
+        button.style.backgroundColor = "#50a84d"
+    }
+}
+
+
+
+
+
+
